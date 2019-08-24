2dcaade836ccbf4018ac840255e31dfda3bb9a4c
Bartoszko <bartek.zawodniak@gmail.com>
2019-08-24 23:46:29 +0200

    fields and constructors added to User entity

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -1,5 +1,32 @@
 package domain;
 
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name="user")
+@Data
 public class User {
 
+	private int id;
+	
+	private String login;
+	
+	private Date joinDate; //TODO add to DB
+	
+	private List<UserStatistic> userStatistics;
+	
+	public User () {}
+	
+	public User (String login, Date date) {
+		this.login = login;
+		this.joinDate = date;
+	}
+	
+	
 }
