f0e9f9433cfef02641d0bb4d22875184fde9fbdd
Bartoszko <bartek.zawodniak@gmail.com>
2019-08-27 02:47:32 +0200

    GameType entity updated

--- src/main/java/domain/GameType.java
+++ src/main/java/domain/GameType.java
@@ -1,5 +1,43 @@
 package domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name="game_type")
+@Data
 public class GameType {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="name")
+	private String typeName;
+	
+	
+	@OneToOne
+	@JoinColumn(name="type_setting_id")
+	private TypeSetting typeSetting;
+	
+	@OneToMany(mappedBy="gameType")
+	private Game game;
+	
+	public GameType() {}
+	
+	public GameType(String typeName, TypeSetting typeSetting) {
+		this.typeName = typeName;
+		this.typeSetting = typeSetting;
+	}
+	
 }
