11c3840e56c21b6951b1b7ca47475acecc680814
Bartoszko <bartek.zawodniak@gmail.com>
2019-09-04 02:01:58 +0200

    Team entity updated

--- src/main/java/domain/Team.java
+++ src/main/java/domain/Team.java
@@ -1,5 +1,47 @@
 package domain;
 
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name="team")
+@Data
 public class Team {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@ManyToMany
+	@JoinTable(name="game_team", 
+	joinColumns=@JoinColumn(name="team_id"),
+	inverseJoinColumns=@JoinColumn(name="game_id"))
+	private List<Game> games;
+	
+	@OneToMany
+	//TODO - add mapping
+	private List<User> users;
+	
+	public Team() {};
+	
+	public Team(String name, List<User> users) {
+		this.name = name;
+		this.users = users;
+	}
 }
