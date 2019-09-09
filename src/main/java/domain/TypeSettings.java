b94b23c74f74e3ff72b442dff0ca3961b040fea9
Bartoszko <bartek.zawodniak@gmail.com>
2019-09-09 23:59:14 +0200

    TypeSettings updated

--- src/main/java/domain/TypeSettings.java
+++ src/main/java/domain/TypeSettings.java
@@ -1,5 +1,38 @@
 package domain;
 
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name="type_settings")
+@Data
 public class TypeSettings {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="key")
+	private String key;
+	
+	@Column(name="value")
+	private String value;
+	
+	@ManyToMany
+	@JoinTable(name="type_property", 
+	joinColumns=@JoinColumn(name="game_type_property_id"),
+	inverseJoinColumns=@JoinColumn(name="game_type_id"))
+	private List<GameType> gameTypes;
 }
