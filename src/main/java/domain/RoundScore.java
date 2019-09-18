e16a541ce0b78d3dd40eebd4b8e4a6096688f489
Bartoszko <bartek.zawodniak@gmail.com>
2019-09-19 00:42:45 +0200

    Round Score entity updated

--- src/main/java/domain/RoundScore.java
+++ src/main/java/domain/RoundScore.java
@@ -1,5 +1,60 @@
 package domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "round_score")
+@Data
 public class RoundScore {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name="points")
+	private int points;
+	
+	@ManyToOne()
+	@JoinColumn(name = "team_id")
+	private Team team;
+
+	@ManyToOne()
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@ManyToOne
+	@JoinColumn(name = "round_id")
+	private Round round;
+
+	public RoundScore() {};
+
+	public RoundScore(Round round, Team team, int points) {
+			this.round = round;
+			this.team = team;
+			this.points = points;
+		}
+	
+	public RoundScore(Round round, User user, int points) {
+		this.round = round;
+		this.user = user;
+		this.points = points;
+	}
+	
+	public RoundScore(Round round, Team team, User user, int points) {
+		this.round = round;
+		this.team = team;
+		this.user = user;
+		this.points = points;
+	}
 
 }
