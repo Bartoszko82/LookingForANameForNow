0095de1ec02ced1045b69208891c609fe0b36aa1
Bartoszko <bartek.zawodniak@gmail.com>
2020-01-30 03:45:46 +0100

    Partially implemented method to save Game to database

---
+++ src/main/java/pl/bartoszko/points/game/GameService.java
@@ -0,0 +1,50 @@
+package pl.bartoszko.points.game;
+
+import java.util.Date;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("gameService")
+public class GameService {
+
+	@Autowired
+	GameDAO gameDAO;
+	
+//	@Autowired
+//	GameTypeService gameTypeService;
+
+	public void addNewGame(String name, Optional<Date> maybeDate, GameType gameType) {
+		Date date = new Date();
+		if (maybeDate.isPresent()) {
+			date = maybeDate.get();
+		} else {
+			date.setTime(System.currentTimeMillis());
+		}
+		
+		// TODO use GameTypeService to get GameType. 
+		// if no GameType found - create new.		
+		
+		// TODO - check for name-date key added in DB - should it be here as well?
+		Game game = new Game(name,date,gameType);
+		gameDAO.saveGame(game);
+	}
+	
+	public void addNewGame(String name, Optional<Date> maybeDate, String gameTypeName) {
+		Date date = new Date();
+		if (maybeDate.isPresent()) {
+			date = maybeDate.get();
+		} else {
+			date.setTime(System.currentTimeMillis());
+		}
+		
+		// TODO use GameTypeService to get GameType. 
+		// if no GameType found - create new.		
+				
+		
+		// TODO - check for name-date key added in DB - should it be here as well?
+//		Game game = new Game(name,date,gameType);
+//		gameDAO.saveGame(game);
+	}
+}
