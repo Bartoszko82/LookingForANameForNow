ce58c58ef51a9ae2a5917292b6965d9dba4ade52
Bartoszko <bartek.zawodniak@gmail.com>
2020-07-25 01:31:18 +0200

    Draft of game facade interface and it`s implementation created

--- src/main/java/pl/bartoszko/points/game/GameFacade.java
+++ src/main/java/pl/bartoszko/points/game/GameFacade.java
@@ -1,10 +1,15 @@
 package pl.bartoszko.points.game;
 
+import java.util.Date;
+
 public interface GameFacade {
 	
 	public void addNewGame(GameDTO gameDTO, GameTypeDTO gameTypeDTO);
 	
-	public GameDTO getGame();
+	public void addNewGame(GameDTO gameDTO, Date date, GameTypeDTO gameTypeDTO);
+	
+//	No getGame method in gameService yet
+//	public GameDTO getGame(int id);
 	
 	public void addNewGameType(GameTypeDTO gameType);
 	
--- src/main/java/pl/bartoszko/points/game/GameFacadeImpl.java
+++ src/main/java/pl/bartoszko/points/game/GameFacadeImpl.java
@@ -1,14 +1,17 @@
 package pl.bartoszko.points.game;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller("gameFacade")
 public class GameFacadeImpl implements GameFacade {
-
+	
 	@Autowired
 	private GameService gameService;
 	
@@ -17,17 +20,26 @@ public class GameFacadeImpl implements GameFacade {
 	
 	@Autowired
 	private ModelMapper modelMapper;
+	
+	protected final Logger logger = LoggerFactory.getLogger(getClass());
 
 	@Override
 	public void addNewGame(GameDTO gameDTO, GameTypeDTO gameTypeDTO) {
-		gameService.addNewGame(gameDTO.getName(), Optional.empty(), convertToGameType(gameTypeDTO));
+		Optional<Date> empty = Optional.empty();
+		gameService.addNewGame(gameDTO.getName(), empty, convertToGameType(gameTypeDTO));
+	}
+	
+	@Override
+	public void addNewGame(GameDTO gameDTO, Date date, GameTypeDTO gameTypeDTO) {
+		gameService.addNewGame(gameDTO.getName(), Optional.of(date), convertToGameType(gameTypeDTO));
 	}
 
-	@Override
-	public GameDTO getGame() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+//No getGame method in gameService yet
+//	@Override
+//	public GameDTO getGame(int id) {
+//		GameDTO gameDTO = convertToGameDTO(gameService.getGame(id));
+//		return gameDTO;
+//	}
 
 	@Override
 	public void addNewGameType(GameTypeDTO gameTypeDTO) {
@@ -49,7 +61,6 @@ public class GameFacadeImpl implements GameFacade {
 		GameDTO gameDTO = modelMapper.map(game, GameDTO.class);
 		return gameDTO;
 	}
-	
 	
 	private GameType convertToGameType(GameTypeDTO gameTypeDTO) {
 		GameType gameType = modelMapper.map(gameTypeDTO, GameType.class);
